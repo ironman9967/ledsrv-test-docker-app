43df14de252cfa1185f43781ed7d6749adadfa6a
Tyson Hester <ironman9967@gmail.com>
2020-12-05 22:09:52 -0500

    added kill signal param

--- src/server/index.js
+++ src/server/index.js
@@ -18,7 +18,7 @@ export const create =  ({
         handler: (req, h) => h.response().code(404)
     })
 
-    process.once('SIGINT', () => {
+    process.once(process.env.AUTO_NPX_CONTAINER_KILL_SIGNAL || 'SIGINT', () => {
         console.log('draining server...')
         server.stop().then(() => {
             console.log('server down')
